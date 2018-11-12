1f88a1f7fffd79540d5f4f83348b66b5c9ba5a6c
roi <rodrinoya.1@gmail.com>
2018-11-12 20:45:13 -0300

    create chofer working

--- objects/chofer.php
+++ objects/chofer.php
@@ -45,4 +45,38 @@ class Chofer{
      
         return $stmt;
     }
+
+    function create(){
+ 
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    apellido=:apellido, nombre=:nombre, documento=:documento, email=:email, vehiculo_id=:vehiculo_id, sistema_id=:sistema_id, created=:created, updated=:updated";
+     
+        $stmt = $this->connection->prepare($query);
+
+        $this->email=htmlspecialchars(strip_tags($this->email));
+        $this->nombre=htmlspecialchars(strip_tags($this->nombre));
+        $this->created=htmlspecialchars(strip_tags($this->created));
+        $this->updated=htmlspecialchars(strip_tags($this->updated));
+        $this->apellido=htmlspecialchars(strip_tags($this->apellido));
+        $this->documento=htmlspecialchars(strip_tags($this->documento));
+        $this->sistema_id=htmlspecialchars(strip_tags($this->sistema_id));
+        $this->vehiculo_id=htmlspecialchars(strip_tags($this->vehiculo_id));
+        
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":updated", $this->updated);
+        $stmt->bindParam(":created", $this->created);
+        $stmt->bindParam(":apellido", $this->apellido);
+        $stmt->bindParam(":documento", $this->documento);
+        $stmt->bindParam(":sistema_id", $this->sistema_id);
+        $stmt->bindParam(":vehiculo_id", $this->vehiculo_id);
+     
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
+         
+    }
 }
