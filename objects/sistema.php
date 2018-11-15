d810adaf9761f8247a2c5e37a5628efabda068a5
roi <rodrinoya.1@gmail.com>
2018-11-15 00:34:17 -0300

    .

--- objects/sistema.php
+++ objects/sistema.php
@@ -37,49 +37,5 @@ class sistema{
         return $stmt;
     }
 
-    function create(){
- 
-        $query = "INSERT INTO
-                    " . $this->table_name . "
-                SET
-                    nombre=:nombre, pais_procedencia=:pais_procedencia, created=:created, updated=:updated";
-     
-        $stmt = $this->connection->prepare($query);
-
-        $this->nombre=htmlspecialchars(strip_tags($this->nombre));
-        $this->created=htmlspecialchars(strip_tags($this->created));
-        $this->updated=htmlspecialchars(strip_tags($this->updated));
-        $this->pais_procedencia=htmlspecialchars(strip_tags($this->pais_procedencia));
-        
-        $stmt->bindParam(":nombre", $this->nombre);
-        $stmt->bindParam(":updated", $this->updated);
-        $stmt->bindParam(":created", $this->created);
-        $stmt->bindParam(":pais_procedencia", $this->pais_procedencia);
-     
-        if($stmt->execute()){
-            return true;
-        }
-        return false;
-         
-    }
-    
-    function delete(){
- 
-        $query = "DELETE FROM " . $this->table_name . " WHERE sistema_id = ?";
-
-        $stmt = $this->connection->prepare($query);
-     
-        $this->sistema_id=htmlspecialchars(strip_tags($this->sistema_id));
-     
-        $stmt->bindParam(1, $this->sistema_id);
-     
-        if($stmt->execute()){
-            return true;
-        }
-     
-        return false;
-         
-    }
-
     
 }
