63defb7bcd18dcb7d9411460824e2bcb1e191437
roi <rodrinoya.1@gmail.com>
2018-11-30 20:21:11 -0300

    chofer read y create listo

--- chofer.php
+++ chofer.php
@@ -47,8 +47,52 @@ if($jwt){
         $usuario= $usu['nombre']." ".$usu['apellido'];
 
         switch ($_SERVER['REQUEST_METHOD']){
+
             case "POST":
-                //create();
+                $chofer->nombre = $data->nombre;
+                $chofer->apellido = $data->apellido;
+                $chofer->documento = $data->documento;
+                $chofer->email = $data->email;
+                $chofer->sistema_id = $data->sistema_id;
+                $chofer->vehiculo_id = $data->vehiculo_id;
+                $chofer->created = date('Y-m-d H:i:s');
+                $chofer->updated = date('Y-m-d H:i:s');
+            
+                if(
+                    !empty($data->nombre) &&
+                    !empty($data->apellido) &&
+                    !empty($data->documento) &&
+                    !empty($data->email) &&
+                    !empty($data->sistema_id) &&
+                    !empty($data->vehiculo_id)
+                ){
+                    
+                    if($chofer->create()){
+                    
+                        $time2= round(((microtime(true) - $time1)*1000), 2);
+                        $auditoria->response_time= $time2;            
+                        $auditoria->usuario= $usuario;
+                        $auditoria->created = date('Y-m-d H:i:s');
+                        //endpoint hay que cambiarlo para cada funcion seria el url de la pagina
+                        $auditoria->endpoint= "localhost/prog1final/chofer/create.php";
+                        //agregar auditoria
+                        $auditoria->create();
+                        
+                        http_response_code(201);
+                        echo json_encode(array("message" => "Agregado correctamente."));
+                    }
+                
+                    else{
+                        http_response_code(503); 
+                        echo json_encode(array("message" => "No se pudo agregar."));
+                    }
+                }
+                //FALTAN DATOS 
+                else{
+                    
+                    http_response_code(400);
+                    echo json_encode(array("message" => "No se pudo agregar, faltan datos."));
+                }
                 break;
                 
             case "GET":
@@ -104,69 +148,19 @@ if($jwt){
                 break;
 
                 
-            }
-            
-            
+        
         }
-        catch (Exception $e){
             
-            http_response_code(401);
-            echo json_encode(array(
-                "message" => "Token invalido.",
-                "error" => $e->getMessage()
-            ));
-        }
+            
+    }catch (Exception $e){
+        
+        http_response_code(401);
+        echo json_encode(array(
+            "message" => "Token invalido.",
+            "error" => $e->getMessage()
+        ));
     }
+}
     
-/*
-function create(){
-        
-    $chofer->nombre = $data->nombre;
-    $chofer->apellido = $data->apellido;
-    $chofer->documento = $data->documento;
-    $chofer->email = $data->email;
-    $chofer->sistema_id = $data->sistema_id;
-    $chofer->vehiculo_id = $data->vehiculo_id;
-    $chofer->created = date('Y-m-d H:i:s');
-    $chofer->updated = date('Y-m-d H:i:s');
-    
-    if(
-        !empty($data->nombre) &&
-        !empty($data->apellido) &&
-        !empty($data->documento) &&
-        !empty($data->email) &&
-        !empty($data->sistema_id) &&
-        !empty($data->vehiculo_id)
-        ){
-            
-            if($chofer->create()){
-                
-            $time2= round(((microtime(true) - $time1)*1000), 2);
-            $auditoria->response_time= $time2;            
-            $auditoria->usuario= $usuario;
-            $auditoria->created = date('Y-m-d H:i:s');
-            //endpoint hay que cambiarlo para cada funcion seria el url de la pagina
-            $auditoria->endpoint= "localhost/prog1final/chofer/create.php";
-            //agregar auditoria
-            $auditoria->create();
-            
-            http_response_code(201);
-            echo json_encode(array("message" => "Agregado correctamente."));
-        }
-        
-        else{
-            http_response_code(503); 
-            echo json_encode(array("message" => "No se pudo agregar."));
-        }
-    }
-    //FALTAN DATOS 
-    else{
-        
-        http_response_code(400);
-        echo json_encode(array("message" => "No se pudo agregar, faltan datos."));
-    }
-}   
-*/
-
 
 ?>
