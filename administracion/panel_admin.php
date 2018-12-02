5a8cce7054196e6fb854af6b2eaf2977d3db639e
Camila <camichavesthg@gmail.com>
2018-12-02 19:44:59 -0300

    panel de administracion

---
+++ administracion/panel_admin.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Panel de Usuarios</title>
+</head>
+<body>
+<h2>Usuarios</h2>
+<a href="add_usu.php" class="btn btn-success">Agregar</a>
+<a href="del_usu.php" class="btn btn-success">Eliminar</a>
+<a href="sear_usu.php" class="btn btn-success">Buscar</a>
+<a href="list_usu.php" class="btn btn-success">Listar</a>
+</body>
+</html>
---
+++ administracion/paneladmin.php
@@ -0,0 +1,15 @@
+
+<html>
+<head> <title>Panel de administracion</title></head>
+<body>
+ 
+
+
+<a href="panel_usu.php">Usuarios</a>
+<a href="panel_audi">Auditoría</a>
+
+
+</body>
+</html>
+
+
