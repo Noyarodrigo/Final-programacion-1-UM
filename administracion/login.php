ab8546360d533bfcee060d9be371f0cc1abb23fb
Camila <camichavesthg@gmail.com>
2018-12-03 22:15:00 -0300

    Login administracion terminado

--- administracion/login.php
+++ administracion/login.php
@@ -1,12 +1,29 @@
-<pre>
-  <h3>Inicio de sesión</h3>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+<body>
+<div class="container" style="background-color:#9fd5d1; " >  <h1 class="text-center" >INICIO DE SESIÓN</h1></div>
+<div class="container " style="background-color:#9fd5d1; ">
   <form action="logincheck.php" method="post">
+    <div class="form-group ">
     <label for="nombre">Nombre </label>
-    <input type="text" name="nombre">
+    <input type="text" name="nombre" class="form-control" placeholder="Ingrese nombre">
+     </div>
+     <div class="form-group">
     <label for="apellido">Apellido </label>
-    <input type="text" name="apellido">
-    <label for="password">Contraseña </label>
-    <input type="password" name="password">
-    <input type="submit" value="Ingresar">
+    <input type="text" name="apellido" class="form-control" placeholder="Ingrese apellido">
+     </div>
+    <div class="form-group">
+      <label for="password">Contraseña:</label>
+      <input type="password" class="form-control" name="password" placeholder="Ingrese contraseña">
+    </div>
+    <button type="submit" class="btn btn-default">Ingresar</button>
   </form>
-</pre>
+</div>
+
+</body> 
+</html>
