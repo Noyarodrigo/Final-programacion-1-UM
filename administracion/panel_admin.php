d4ad2af88daeedfe5c1411d9f3cbb61a57c36a26
roi <rodrinoya.1@gmail.com>
2018-12-03 23:49:10 -0300

    a ver si lo arregle

--- administracion/panel_admin.php
+++ administracion/panel_admin.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Auditoria</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="login/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="login/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
 <?php
 if (session_start()) {
     if ($_SESSION[habilitado]!=1 && $_SESSION[rol]!="admin") {
