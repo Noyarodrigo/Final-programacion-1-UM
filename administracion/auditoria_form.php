29a4f7526ebe322a015962cfb8a6587e555971be
roi <rodrinoya.1@gmail.com>
2018-12-03 22:57:23 -0300

    ...

--- administracion/auditoria_form.php
+++ administracion/auditoria_form.php
@@ -1,12 +1,83 @@
-<html>
-<head> <title>REGISTRO</title></head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Auditoria</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="login/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="login/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login/css/main.css">
+<!--===============================================================================================-->
+</head>
 <body>
-<form action="auditoria_listar.php" method="post">
-    <div>Fecha Inicial</div>
-    <div> <input type="text" name="fi"></div>
-    <div>Fecha Final</div>
-    <div> <input type="text" name="ff"></div>
-    <input type="submit" value="Listar" >
-</form>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 row justify-content-center align-items-center">
+				
+				<form action="add_usu2.php" class="login100-form validate-form" method="post">
+					<span class="login100-form-title">
+						Ingrese las fechas o dejelas vacias para mostrar todo
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Debe ingresar su apellido">
+						<input class="input100" type="text" name="ape" placeholder="Apellido">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Ingrese su contraseña">
+						<input class="input100" type="password" name="cla" placeholder="Contraseña">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Agregar
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
--- administracion/auditoria_listar.php
+++ administracion/auditoria_listar.php
@@ -1,53 +1,104 @@
-<?php
-if (session_start()) {
-    if ($_SESSION[habilitado]!=1 && $_SESSION[rol]!="admin") {
-      //a la casa a loguearse
-      header("location: http://localhost/prog1final/administracion/login.php");
-      die();
-    }
-  }
-$usuario="root";
-$clave="1234";
-$bd="transporte";
-$servidor="localhost";
-$conexionPDO= new PDO("mysql:host=$servidor;dbname=$bd;charset=UTF8","$usuario","$clave");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Auditoria</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="login/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="login/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 row justify-content-center align-items-center">
+                <span class="login100-form-title">
+                   Auditoria
+                </span>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nombre y Apellido</th>
+                            <th scope="col">Tpo Respuesta</th>
+                            <th scope="col">Endpoint</th>
+                            <th scope="col">Created</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        if (session_start()) {
+                            if ($_SESSION[habilitado]!=1 && $_SESSION[rol]!="admin") {
+                            //a la casa a loguearse
+                            header("location: http://localhost/prog1final/administracion/login.php");
+                            die();
+                            }
+                        }
+                        $usuario="root";
+                        $clave="1234";
+                        $bd="transporte";
+                        $servidor="localhost";
+                        $conexionPDO= new PDO("mysql:host=$servidor;dbname=$bd;charset=UTF8","$usuario","$clave");
 
-$fi=isset($_POST["fi"]) ? $_POST["fi"] : "";
-$ff=isset($_POST["ff"]) ? $_POST["ff"] : "";
+                        $fi=isset($_POST["fi"]) ? $_POST["fi"] : "";
+                        $ff=isset($_POST["ff"]) ? $_POST["ff"] : "";
 
-if ( $fi!="" && $ff!="") {
-    
-    $sql="select * from auditoria where created between \"$fi\" and \"$ff\"";
-    
-}else{
+                        if ( $fi!="" && $ff!="") {
+                            
+                            $sql="select * from auditoria where created between \"$fi\" and \"$ff\"";
+                            
+                        }else{
 
-    $sql="select * from auditoria ";
+                            $sql="select * from auditoria ";
 
-}
+                        }
 
+                        $ejecucionSQLPDO=$conexionPDO->prepare($sql);
+                        $ejecucionSQLPDO->execute();
 
-$ejecucionSQLPDO=$conexionPDO->prepare($sql);
-$ejecucionSQLPDO->execute();
-echo "<div> Auditoria </div>";
-echo "<table border='1'>";
-echo"<tr> <td>ID</td> <td>Nombre y Apellido</td><td>Tpo de respuesta</td> <td>Endpoint</td> <td>Created</td> </tr>";
+                        $path="registro.txt";
+                        $archivo=fopen($path,'w');
 
-$path="registro.txt";
-$archivo=fopen($path,'w');
-$i=0;
-while($filaPDO=$ejecucionSQLPDO->fetch(PDO::FETCH_ASSOC)){
-    echo "<tr>";
-    foreach ($filaPDO as $campoPDO){
-        echo "<td>$campoPDO</td>";
-        $array[$i][]=$campoPDO;
-    }
-    $texto = $array[$i][0].",".$array[$i][1].",".$array[$i][2].",".$array[$i][3].",".$array[$i][4]."\n";
-    fwrite($archivo,$texto);    
-    echo "</tr>";
-    $i++;
-}
-echo "</table> ";
-echo "<form action=\"auditoria_descargar.php\" method=\"post\"><br><input type=\"submit\" value=\"Descargar Registro\"></form>";
-fclose($archivo);
+                        $i=0;
 
-?>
+                        while($filaPDO=$ejecucionSQLPDO->fetch(PDO::FETCH_ASSOC)){
+                            echo "<tr>";
+                            foreach ($filaPDO as $campoPDO){
+                                echo "<td>$campoPDO</td>";
+                                $array[$i][]=$campoPDO;
+                            }
+                            $texto = $array[$i][0].",".$array[$i][1].",".$array[$i][2].",".$array[$i][3].",".$array[$i][4]."\n";
+                            fwrite($archivo,$texto);    
+                            echo "</tr>";
+                            $i++;
+                        }
+
+                        fclose($archivo);
+                        ?>
+                    </tbody>
+                </table>
+				
+                <form action="auditoria_descargar.php" class="login100-form validate-form" method="post">
+                    <div class="container-login100-form-btn">
+                            <button class="login100-form-btn">
+                                Descargar
+                            </button>
+                    </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
